02925c6349a1ee60094c1754d03a2c7b829bc099
areznawzad <101289516+areznawzad@users.noreply.github.com>
2023-08-01 18:19:00 +0300

    Add files via upload

---
+++ arezcv.php
@@ -0,0 +1,161 @@
+
+<style>
+    body{
+        margin: 200px;
+        padding: 250px;
+    }
+    h1,h2,h5,h6{
+        text-align: center;
+    }
+    .p1{
+      text-align: center;  
+    }
+   
+    .rounded-circle {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 35%;
+}
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    
+</head>
+<body>
+
+<div class="container-fluid">
+  <h1>AREZ NAWZAD</h1>
+     <h5>Information Technology</h5>
+    <div class="container mt-4">
+    <img class="rounded-circle" src="1.png" width="304" height="236">
+    </div><br>
+ <h2>PROFILE</h2><br>
+  <p class="p1">Address: Sulaymaniah <br>
+Birth: 14/6/2000 <br>
+Nationality: Iraqi – Kurdish<br>
+Graduation Year: 2020</p><br>
+    <h6>EDUCATION</h6><br>
+    <p class="p1">Kurdistan Technical Institute <br> [Information Technology (IT).]
+</p><br>
+     <h3>WORK EXPERIENCE
+</h3><br>
+    <p >
+    <dl>
+   <dt>IT Worker:</dt>  <br>
+1.Network work  <br> 2.website creation <br> 3.design and video editing <br>
+<dt>Teacher:</dt><br>
+1.Computer Repairs Teacher.<br>
+2.Excel & Access Teacher.<br>
+3.Web Designs Teacher (Online) {Html – Css – Php – Bootstrap – Js} <br>
+<dt>Secretery:</dt> <br>
+1. Secretery And Nursery at Dental Clinic <br>
+2.Accountant and telephone work at company<br>
+<dt>Artist:</dt><br>
+1.Drawing and special lessons for children <br>
+2.Ceramic handicraft <br>
+3.carpenter <br>
+4.carpet knitting and wool <br>
+5.open a special exhibitions in sulaymaniah as a charity work to help 
+poor children and peshmarga (selling handicraft) <br>
+6.Makeup Artist <br>
+<dt>Create Website:</dt> <br>
+http://cvbyarez.epizy.com/cv.php  <a href="http://cvbyarez.epizy.com/cv.php" class="btn btn-info" role="button">Click Here</a> <br>
+https://areznawzad.github.io/platformgame/ <a href="https://areznawzad.github.io/platformgame/" class="btn btn-info" role="button">Click Here</a><br><br>
+        <dt>The Book Writer</dt>
+    </dl>
+    <br>
+    <h3>SKILLS</h3>
+    <p>Enterprise System<br> Advertising <br>IT Support <br>Leadship <br>Presentations
+    </p><br>
+    
+    <h3>COMPUTER SKILLS</h3>
+    <dt>1. Graphical</dt> 
+  Photoshop<br>
+  Illustrator<br>
+  InDesign<br>
+ Corel Draw<br>
+ Sketch<br>
+ Paint 3D<br>
+    
+    <br><br>
+   <dt>2. Microsoft</dt> 
+ Word <br>
+ Excel <br>
+ Access<br>
+ Power Point <br>
+ Visio <br>
+Project <br>
+Publisher
+    
+    <br><br><br>
+    <dt>3. Web Pentester</dt><br>
+     <div class="container mt-4">
+    <img src="2.png" width="304" height="236">
+    </div><br><br><br>
+    
+   <dt>4. Application Software Skills</dt>
+     <div class="container mt-4">
+    <img src="3.png" width="304" height="236">
+    </div><br><br><br>
+    
+    
+    <dt>5. Hardware Reparation</dt>
+    <div class="container mt-4">
+    <img src="4.jpg" width="304" height="236">
+        <img src="5.png" width="304" height="236">
+    </div><br><br><br>
+    
+    <dt>6. Social Media</dt>
+      <div class="container mt-4">
+    <img src="6.png" width="304" height="236">
+    </div><br><br><br>
+    
+    
+    <dt>7. Writing</dt>
+ Typing….<br><br><br>
+    
+    
+    <dt>8. Web Design ---- MySql</dt>
+<div class="container mt-4">
+    <img src="7.png" width="390" height="236">
+        <img src="8.jpg" width="390" height="236">
+    </div><br><br><br>
+    
+    
+    <dt>9.Database Management</dt><br><br><br>
+    <dt>10. Windows</dt><br><br><br>
+    
+    
+    <dt>Courses</dt>
+    Project Management Professional (PMP)<br>
+Ethical Hacking & Cyber Security<br>
+Design System Bank in Adobe XD<br>
+Hardware Reparation { Computer Cases - solder machine – Check parts –
+printer repair - Paste Parts Motherboard }<br>
+Wordpress
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+    
+    
+    
+    
+    </body></html>
+    
+
